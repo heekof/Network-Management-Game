392603a97a5010fa816a4bf972411b43c2a845ee
Jaafar <bendrisj2@gmail.com>
2016-11-29 22:38:34 +0100

    Adding Service Definiton and SLA

--- Network.py
+++ Network.py
@@ -1,6 +1,11 @@
 
 # Python Network Simulation for Q-Learning
+# Now you need a UML Diagram
 
+import numpy as np
+
+
+# This is the skeleton of the Network
 class Network:
 
     totalTime = 1
@@ -37,14 +42,29 @@ class Network:
         linkInstantiationTime = 5
         linkUpdateNonAvailability = 3
 
-class Resource:
+class SLA:
+    def __init__(self):
+        pass
 
+    class SLO:
+        def __init__(self):
+            pass
+
+# This is the service on top of the network
+class Service:
+    def __init__(self):
+        pass
+
+
+class Resource:
+    delay = 2
     def __init__(self):
         pass
 
 class CPU1(Resource):
     price = 1
     performance = 1
+
     def __init__(self):
         pass
 
@@ -61,21 +81,46 @@ class CPU3(Resource):
         pass
 class Flow:
     sessionNumber = 0
-    def __init__(self):
-        pass
+    def __init__(self,difficulty):
+        if difficulty == "easy":
+            pass
+        elif difficulty == "normal":
+            pass
+        else:
+            True
+
+
     def flowGenerator(self):
         pass
 
+class NetGame:
+
+    level = ""
+
+    def __init__(self):
+        pass
+
+    def setDifficultyLevel(self,level):
+        self.level = level
+        print(self.level)
 
 
 if __name__ == '__main__':
     print('This is the main program')
 
 
+# Instanciate the Network with a name, a number of router, a number of servers and a budget
 
-emp_1 = Network('Network 1', 3, 5, 100)
+MyNet = Network('Network 1', 3, 5, 100)
+MyFlow = Flow("easy");
 
+Game = NetGame()
+
+Game.setDifficultyLevel("easy")
 
 print(Network.__dict__)
 
-print(emp_1.__dict__)
+print(MyNet.__dict__)
+
+print(Game.__dict__)
+
